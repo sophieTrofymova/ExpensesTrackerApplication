026b896e4a6aec5944082d0f3e99c11f795268db
Sonya <Sonya@DESKTOP-CH8DTKE>
2025-04-06 21:58:58 +0100

    commits

---
+++ ExpenseTracker/Controls/AddTransactionElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpenseTracker.Controls
+{
+    public partial class AddTransactionElement: Element
+    {
+        public AddTransactionElement()
+        {
+            InitializeComponent();
+        }
+    }
+}
