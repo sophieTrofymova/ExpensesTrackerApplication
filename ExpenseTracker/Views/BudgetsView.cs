e055a1aabaf81670ca414b77e58551e0a11f3102
EllenRinne <raigneier@gmail.com>
2025-04-10 23:18:38 +0100

    Refactor BudgetsView and add new view screen builder
    Reformat namespace declarations to single-line braces.
    Initialize NumRows and NumCols to 1 in BudgetsView constructor.
    Move Padding initialization inside the constructor.
    Remove Build method and integrate its functionality into constructor.
    Update BudgetsElement initialization: replace ThemedGroupBox with Title, add Row property.
    Add new screen builder for "view" screen with budgetsElement.
    Set DefaultScreenKey to "view".

--- ExpenseTracker/Views/BudgetsView.cs
+++ ExpenseTracker/Views/BudgetsView.cs
@@ -3,39 +3,34 @@ using ExpenseTracker.Controls;
 using ExpenseTracker.Elements;
 using WF = System.Windows.Forms;
 
-namespace ExpenseTracker.Views
-{
+namespace ExpenseTracker.Views {
 
-    namespace ExpenseTracker
-    {
+    namespace ExpenseTracker {
         public class BudgetsView : ElementView {
-            public BudgetsView(ElementContainer container) : base(container) 
-            {
-                this.NumRows = 10;
-                this.NumCols = 20;
-
-                Padding = new System.Windows.Forms.Padding(25);
-            }
-
-            public override void Build() {
-                var user = App.State.UserManager.LoggedUser;
-                ClearElements();
-                this.NumCols = 1;
+            public BudgetsView(ElementContainer container) : base(container) {
                 this.NumRows = 1;
+                this.NumCols = 1;
 
                 var budgetsElement = new BudgetsElement(this) {
-                    ThemedGroupBox = { Text = "Budgets" },
+                    Title = "Budgets",
                     Col = 0,
+                    Row = 0,
                     Cols = 1,
                     Rows = 1,
                     AllowDrag = true
                 };
+
                 screenBuilders.Add("view", () => new List<Element> { budgetsElement });
 
                 Padding = new System.Windows.Forms.Padding(25);
 
                 DefaultScreenKey = "view";
+
+
             }
+
+
+
 
         }
 
